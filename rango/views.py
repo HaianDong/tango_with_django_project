a78e157a3df4f7017ce7b5eabd91b239b6f71386
Haian Dong <2546659d@student.gla.ac.uk>
2021-07-27 22:07:01 +0800

    chapter4

--- rango/views.py
+++ rango/views.py
@@ -2,13 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    response = HttpResponse()
-    response.write("<p>Rango says hey there partner!</p>")
-    response.write("<a href='/rango/about/'>About</a>")
-    return response
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    response = HttpResponse()
-    response.write("<p>Rango says here is the about page.</p>")
-    response.write("<a href='/rango/'>Index</a>")
-    return response
+    context_dict = {'boldmessage': 'This tutorial has been put together by Haian.'}
+    return render(request, 'rango/about.html', context=context_dict)
